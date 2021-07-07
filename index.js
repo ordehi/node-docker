04903ca76dc4a8d5dae8ada68ea5ec07cefbecde
Joshua Ordehi <mev.dev.kar@gmail.com>
2021-07-06 22:08:51 -0400

    added signup, login, nginx for load balancing + cors. ready to start setting up prod

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const redis = require("redis");
+const cors = require("cors");
+let RedisStore = require("connect-redis")(session);
+
 const {
     MONGO_USER,
     MONGO_PASSWORD,
     MONGO_IP,
     MONGO_PORT,
+    REDIS_URL,
+    REDIS_PORT,
+    SESSION_SECRET,
 } = require("./config/config");
+
+let redisClient = redis.createClient({
+    host: REDIS_URL,
+    port: REDIS_PORT,
+});
 
 const postRouter = require("./routes/postRoutes");
 const userRouter = require("./routes/userRoutes");
@@ -28,11 +41,27 @@ const connectWithRetry = () => {
         });
 };
 
-app.get("/", (req, res) => {
+app.get("/api/v1", (req, res) => {
     res.send("<h2>We're up!!!</h2>");
 });
 
 connectWithRetry();
+
+app.enable("trust proxy");
+app.use(cors({}));
+app.use(
+    session({
+        store: new RedisStore({ client: redisClient }),
+        secret: SESSION_SECRET,
+        cookie: {
+            secure: false,
+            resave: false,
+            saveUninitialized: false,
+            httpOnly: true,
+            maxAge: 6000000, // * testing time
+        },
+    })
+);
 
 app.use(express.json());
 
