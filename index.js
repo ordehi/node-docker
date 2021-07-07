f88628cee748f7eecc80bebae2d8aa517b332ebc
Joshua Ordehi <mev.dev.kar@gmail.com>
2021-07-06 20:03:33 -0400

    crud done, adding bcrypt next

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const {
-  MONGO_USER,
-  MONGO_PASSWORD,
-  MONGO_IP,
-  MONGO_PORT,
+    MONGO_USER,
+    MONGO_PASSWORD,
+    MONGO_IP,
+    MONGO_PORT,
 } = require("./config/config");
+
+const postRouter = require("./routes/postRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 
 const connectWithRetry = () => {
-  mongoose
-    .connect(mongoURL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    })
-    .then(() => console.log("connected to DB"))
-    .catch((e) => {
-      console.error(e);
-      setTimeout(connectWithRetry, 5000);
-    });
+    mongoose
+        .connect(mongoURL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+        })
+        .then(() => console.log("connected to DB"))
+        .catch((e) => {
+            console.error(e);
+            setTimeout(connectWithRetry, 5000);
+        });
 };
 
 app.get("/", (req, res) => {
-  res.send("<h2>We're up!!!</h2>");
+    res.send("<h2>We're up!!!</h2>");
 });
 
 connectWithRetry();
 
+app.use(express.json());
+
+app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/users", userRouter);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`listening on port ${port}`));
